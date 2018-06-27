c67b7de2c50c6c49c4e90718f18293082f88bd73
Vishal <justmailvishal58@icloud.com>
2018-06-27 20:43:40 +0000

    just checking

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world welcome from cloud 9");
+    int a;
+    printf("Enter a number=");
+    scanf("%d",&a);
+    
+    printf("The Number is %d",a);
+    
+    return 0;
+}
